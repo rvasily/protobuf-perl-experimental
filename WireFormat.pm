12f9014ef9b9feafc54540a14010b192c23b6cec
Pete Keen <pak@rentrak.com>
2009-10-23 17:05:36 -0700

    read_tag() actually *was* working, but I was trying to write values bigger than 3 bits into the wire_type field

--- WireFormat.pm
+++ WireFormat.pm
@@ -1,15 +1,29 @@
 package WireFormat;
 
+use strict;
+
 use base 'Exporter';
-use vars @EXPORT_OK;
+our @EXPORT_OK;
+
 BEGIN {
     push @EXPORT_OK, qw/
         read_varint
         write_varint
+        WIRE_TYPE_VARINT
+        WIRE_TYPE_64BIT
+        WIRE_TYPE_LENGTH_DELIM
+        WIRE_TYPE_START_GROUP
+        WIRE_TYPE_END_GROUP
+        WIRE_TYPE_32BIT
     /;
 }
 
-use strict;
+use constant WIRE_TYPE_VARINT       => 0;
+use constant WIRE_TYPE_64BIT        => 1;
+use constant WIRE_TYPE_LENGTH_DELIM => 2;
+use constant WIRE_TYPE_START_GROUP  => 3;
+use constant WIRE_TYPE_END_GROUP    => 4;
+use constant WIRE_TYPE_32BIT        => 5;
 
 sub write_varint
 {
--- test-message.pl
+++ test-message.pl
@@ -2,7 +2,7 @@
 
 use strict;
 use lib '.';
-use WireFormat qw/ read_varint write_varint /;
+use WireFormat qw/ read_varint write_varint WIRE_TYPE_VARINT /;
 
 sub read_tag
 {
@@ -11,15 +11,14 @@ sub read_tag
     my $handle = shift;
     my $tag = read_varint($handle);
     my $wire_type = $tag & $wire_type_mask;
-    my $tag_no = $tag >> 3;
-    return ($tag_no, $wire_type);
+    my $field_number = $tag >> 3;
+    return ($field_number, $wire_type);
 }
 
 sub write_tag
 {
-    my ($handle, $tag_no, $wire_type) = @_;
-    my $tag = 
-    return write_varint($handle, ($tag_no << 3) | $wire_type);
+    my ($handle, $field_number, $wire_type) = @_;
+    return write_varint($handle, ($field_number << 3) | $wire_type);
 }
 
 use IO::Scalar;
@@ -28,7 +27,7 @@ use Data::Dumper;
 my $scalar = '';
 my $handle = IO::Scalar->new(\$scalar);
 
-write_tag($handle, 10, 9);
+write_tag($handle, 10, WIRE_TYPE_VARINT);
 print 'scalar: ' . unpack('B*', $scalar) . "\n";
 $handle->seek(0,0);
 
